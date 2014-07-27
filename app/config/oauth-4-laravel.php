39c07b151059b307cd573ce2cb4de528feb690c7
Alexander Galushka <apgalushka@ra.rockwell.com>
2014-07-27 16:12:00 -0400

    add OAuth

---
+++ app/config/oauth-4-laravel.php
@@ -0,0 +1,30 @@
+return array( 
+
+    /*
+    |--------------------------------------------------------------------------
+    | oAuth Config
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Storage
+     */
+    'storage' => 'Session', 
+
+    /**
+     * Consumers
+     */
+    'consumers' => array(
+
+        /**
+         * Facebook
+         */
+        'Facebook' => array(
+            'client_id'     => '',
+            'client_secret' => '',
+            'scope'         => array(),
+        ),      
+
+    )
+
+);
