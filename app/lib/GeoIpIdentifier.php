8413a648170cf2670fee33337aa5e350b00cd533
Alexander Galushka <apgalushka@ra.rockwell.com>
2014-08-07 01:31:58 -0400

    adding dynamic forms to /indeed page

---
+++ app/lib/GeoIpIdentifier.php
@@ -0,0 +1,55 @@
+<?php
+
+// to get "real IP", avoiding proxies,
+// some of the code is taken from: http://stackoverflow.com/questions/13646690/how-to-get-real-ip-from-visitor
+
+include(base_path().'\vendor\autoload.php');
+use GeoIp2\Database\Reader;
+use MaxMind\Db\Reader\InvalidDatabaseException;
+	
+class GeoIpIdentifier
+{
+  
+	private $reader;
+	
+	public function __construct()
+	{   
+		// This creates the Reader object, which should be reused across lookups.
+		$this->reader = new Reader(base_path().'/public/geoLiteDB/GeoLite2-City.mmdb');
+    }
+
+	public function GetUserLocation()
+	{
+		try
+		{
+			$result = $this->reader->city($this->getUserIP());
+		}
+		catch (Exception $e)
+		{
+			$result = $this->reader->city('67.134.204.13'); //defaults to Cambridge, MA
+		}
+		return $result;
+	}
+	
+	static function getUserIP()
+	{
+		$client  = @$_SERVER['HTTP_CLIENT_IP'];
+		$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+		$remote  = $_SERVER['REMOTE_ADDR'];
+
+		if(filter_var($client, FILTER_VALIDATE_IP))
+		{
+			$ip = $client;
+		}
+		elseif(filter_var($forward, FILTER_VALIDATE_IP))
+		{
+			$ip = $forward;
+		}
+		else
+		{
+			$ip = $remote;
+		}
+		return $ip;
+	}
+
+}	
--- app/views/indeed.blade.php
+++ app/views/indeed.blade.php
@@ -17,75 +17,38 @@
 	@endforeach
 		  
 		  
-	<p><?php
-	
+	<p><?php	
 	$result= Session::get('data');
+	$skills = $result['skillSet'];	
+	?> </p>	
 	
-	print_r ($result);
+    <p style="font-size:24px">Please pick up to 6 relevant skills to the job you would like to search for from the form below:</>
 
-	echo "<br><br>";
+	<fieldset>
+	<legend>Skill Set provided via LinkedIn</legend>
+		{{ Form::open(array('url' => '/indeed', 'class' => 'navbar-form navbar-left')) }}		
+			@for ($i = 0; $i < count($skills); $i++)
+				@if ($i%5 == 0)
+				    @if ($i != 0)
+						{{"<br><br>"}}
+					@endif
+				@endif	
+				{{ Form::checkbox($skills[$i], 'yes') }}
+				{{ Form::label($skills[$i]) }}
+			@endfor
+		{{"<br><br>"}}
+		{{ Form::submit('Choose', array('class' => 'btn btn-dafault')) }}
+		{{ Form::close() }}	
+	</fieldset>
+
 	
-	//to get "real IP", avoiding proxies, code is taken from: http://stackoverflow.com/questions/13646690/how-to-get-real-ip-from-visitor
-	function getUserIP()
-	{
-		$client  = @$_SERVER['HTTP_CLIENT_IP'];
-		$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-		$remote  = $_SERVER['REMOTE_ADDR'];
-
-		if(filter_var($client, FILTER_VALIDATE_IP))
-		{
-			$ip = $client;
-		}
-		elseif(filter_var($forward, FILTER_VALIDATE_IP))
-		{
-			$ip = $forward;
-		}
-		else
-		{
-			$ip = $remote;
-		}
-
-		return $ip;
-	}
-
-
-	$user_ip = getUserIP();
+	{{"<br><br>"}}
 	
-	include(base_path().'\vendor\autoload.php');
-	use GeoIp2\Database\Reader;
-	use MaxMind\Db\Reader\InvalidDatabaseException;
-
-	// This creates the Reader object, which should be reused across
-	// lookups.
-	$reader = new Reader(base_path().'/public/geoLiteDB/GeoLite2-City.mmdb');
-	
-
-	// Replace "city" with the appropriate method for your database, e.g.,
-	// "country".
-	//$record = $reader->city('67.134.204.13'); //for localhost - testing purposes
-	try
-	{
-		$record = $reader->city($user_ip);
-	}
-	catch (Exception $e)
-	{
-		$record = $reader->city('67.134.204.13'); //defaults to Cambridge, MA
-	}
-
-	print($record->country->isoCode . "\n"); // 'US'
-	print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MA'
-	print($record->city->name . "\n"); // 'Cambridge'
-	print($record->postal->code . "\n"); // '02138'
-
-	//array_key_exists
-	//[educations] => stdClass Object ( [_total] => 3 [values]
-	
-
-	?> </p>		  
-	
+	<fieldset>
+	<legend>Another form</legend>
 	{{ Form::open(array('url' => '/indeed', 'class' => 'navbar-form navbar-left')) }}
 				
-		{{ Form::text('jobTitle', '', array('class' => 'form-control', 'placeholder' => 'Job Title')) }}<br><br>
+		{{ Form::text('jobTitle', 'Mama', array('class' => 'form-control', 'placeholder' => 'Job Title')) }}<br><br>
 	
 		{{ Form::text('country', '', array('class' => 'form-control', 'placeholder' => 'Country')) }}<br><br>
 		
@@ -94,6 +57,7 @@
 		{{ Form::submit('Submit', array('class' => 'btn btn-dafault')) }}
 	
 	{{ Form::close() }}
+	</fieldset>
 
 	
 @stop
